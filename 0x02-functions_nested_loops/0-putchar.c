5a98273165739e8dbcb6d6743a49fb44a24bb7ff
lordteekay <tokunboad@gmail.com>
2022-09-15 15:42:04 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,19 @@
-#include <stdio.h>
-/*main - Entry point,and the program must return integer 0*/
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Alwats returns 0
+ */
 int main(void)
 {
-	printf("_putchar\n");
-	return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }

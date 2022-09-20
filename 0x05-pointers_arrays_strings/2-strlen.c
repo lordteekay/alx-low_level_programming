e1c861bf72e37b6085fe44ca7d2c1bcf78d8995d
lordteekay <tokunboad@gmail.com>
2022-09-20 13:44:02 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+ * _strlen - returns the length of a string
+ * @s: arrays of characters or string
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+	int length = 0;
+
+	for (i = 0; *s[i] != '\0'; i++)
+		length++;
+	return (lenght);
+}

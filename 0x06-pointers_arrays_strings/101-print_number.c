7dfc6baf3d66de93dd0dd0e981fffb8ca7b9e37a
lordteekay <tokunboad@gmail.com>
2022-09-23 16:03:50 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,17 +4,18 @@
  * @n: integer to be printed
  * Return: void
  */
+
 void print_number(int n)
 {
 	unsigned int num = n;
 
 	if (n < 0)
 	{
-		_putchar(*-*);
+		_putchar('-');
 		num = -num;
 	}
 
-	if ((num / 10) >)
+	if ((num / 10) > 0)
 		print_number(num / 10);
 
 	_putchar((num % 10) + '0');
